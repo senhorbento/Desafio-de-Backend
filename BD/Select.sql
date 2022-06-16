6c8abab3826671ce41853be225968448f983dbe5
senhorbento <sbento.ti@gmail.com>
2022-06-16 10:47:53 -0300

    Feat: Add consulta total de pagamento por boleto em 2022

--- BD/Select.sql
+++ BD/Select.sql
@@ -1,84 +1,98 @@
 -- Usado MySQL
+-- Total  de pagamentos utilizando boleto por mês de 20222
+
 USE desafio;
 
-(SELECT "Janeiro" as mês, sum(valor) as total 
+(SELECT "Janeiro" as mês, 
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total 
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-01-01' 
 AND '2022-01-31')
 
 UNION ALL
 
-(SELECT "Fevereiro" as mês, sum(valor) as total 
+(SELECT "Fevereiro" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-02-01' 
 AND '2022-02-28')
 
 UNION ALL
 
-(SELECT "Março" as mês, sum(valor) as total 
+(SELECT "Março" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-03-01' 
 AND '2022-03-31')
 
 UNION ALL
 
-(SELECT "Abril" as mês, sum(valor) as total 
+(SELECT "Abril" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-04-01' 
 AND '2022-04-30')
 
 UNION ALL
 
-(SELECT "Maio" as mês, sum(valor) as total 
+(SELECT "Maio" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-05-01' 
 AND '2022-05-31')
 
 UNION ALL
 
-(SELECT "Junho" as mês, sum(valor) as total 
+(SELECT "Junho" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-06-01' 
 AND '2022-06-30')
 
 UNION ALL
 
-(SELECT "Julho" as mês, sum(valor) as total 
+(SELECT "Julho" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-07-01' 
 AND '2022-07-31')
 
 UNION ALL
 
-(SELECT "Agosto" as mês, sum(valor) as total 
+(SELECT "Agosto" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-08-01' 
 AND '2022-08-31')
 
 UNION ALL
 
-(SELECT "Setembro" as mês, sum(valor) as total 
+(SELECT "Setembro" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-09-01' 
 AND '2022-09-30')
 
 UNION ALL
 
-(SELECT "Outubro" as mês, sum(valor) as total 
+(SELECT "Outubro" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-10-01' 
 AND '2022-10-31')
 
 UNION ALL
 
-(SELECT "Novembro" as mês, sum(valor) as total 
+(SELECT "Novembro" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-11-01' 
 AND '2022-11-30')
 
 UNION ALL
 
-(SELECT "Dezembro" as mês, sum(valor) as total 
+(SELECT "Dezembro" as mês,
+CAST((sum(valor)/100) AS DECIMAL(10,2)) as total  
 FROM PagamentoBoletos
 WHERE data_pagamento BETWEEN '2022-12-01' 
 AND '2022-12-31');
